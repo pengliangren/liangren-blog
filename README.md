8356b76a41e2a7f1a01e65d1fa654a8f97e5afa4
pengliangren <13763053437@163.com>
2022-04-28 17:42:05 +0800

    删除gh-pages 分支代码

--- README.md
+++
@@ -1,6 +0,0 @@
-## liangren-blog
-个人博客
-
-
-## 预览地址
-- github page : <https://pengliangren.github.io/liangren-blog/>
--- docs/.vitepress/theme/index.js
+++
@@ -1,5 +0,0 @@
-import DefaultTheme from 'vitepress/theme';
-// import 'vitepress-theme-demoblock/theme/styles/index.css'
-import './custom.css';
-
-export default DefaultTheme;
